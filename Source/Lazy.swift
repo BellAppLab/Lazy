529ccb3f6e0ee14673556193d4ee218854acef15
Bell App Lab <apps@bellapplab.com>
2018-01-31 13:59:12 +0100

    Bump to v0.2.1; better multithreading I hope

--- Source/Lazy.swift
+++ Source/Lazy.swift
@@ -1,15 +1,4 @@
 //
-//  Lazy.swift
-//  Example
-//
-//  Created by Bell App Lab on 21.12.17.
-//  Copyright © 2017 Bell App Lab. All rights reserved.
-//
-
-import Foundation
-
-
-//
 //  CwlMutex.swift
 //  CwlUtils
 //
@@ -34,70 +23,30 @@ import Foundation
     import Darwin
 #endif
 
-/// A basic mutex protocol that requires nothing more than "performing work inside the mutex".
-public protocol ScopedMutex {
-    /// Perform work inside the mutex
-    func sync<R>(execute work: () throws -> R) rethrows -> R
-    func trySync<R>(execute work: () throws -> R) rethrows -> R?
-}
-
-/// A more specific kind of mutex that assume an underlying primitive and unbalanced lock/trylock/unlock operators
-public protocol RawMutex: ScopedMutex {
-    associatedtype MutexPrimitive
-    
-    /// The raw primitive is exposed as an "unsafe" public property for faster access in some cases
-    var unsafeMutex: MutexPrimitive { get set }
-    
-    func unbalancedLock()
-    func unbalancedTryLock() -> Bool
-    func unbalancedUnlock()
-}
-
-extension RawMutex {
-    /** RECOMMENDATION: until Swift can inline between modules or at least optimize @noescape closures to the stack, if this file is linked into another compilation unit (i.e. linked as part of the CwlUtils.framework but used from another module) it might be a good idea to copy and paste the relevant `fastsync` implementation code into your file (or module and delete `private` if whole module optimization is enabled) and use it instead, allowing the function to be inlined.
-     ~~~
-     private extension UnfairLock {
-     func fastsync<R>(execute work: @noescape () throws -> R) rethrows -> R {
-     os_unfair_lock_lock(&unsafeLock)
-     defer { os_unfair_lock_unlock(&unsafeLock) }
-     return try work()
-     }
-     }
-     private extension PThreadMutex {
-     func fastsync<R>(execute work: @noescape () throws -> R) rethrows -> R {
-     pthread_mutex_lock(&unsafeMutex)
-     defer { pthread_mutex_unlock(&unsafeMutex) }
-     return try work()
-     }
-     }
-     ~~~
-     */
-    public func sync<R>(execute work: () throws -> R) rethrows -> R {
+fileprivate final class PThreadMutex {
+    func sync<R>(execute work: () throws -> R) rethrows -> R {
         unbalancedLock()
         defer { unbalancedUnlock() }
         return try work()
     }
-    public func trySync<R>(execute work: () throws -> R) rethrows -> R? {
+    func trySync<R>(execute work: () throws -> R) rethrows -> R? {
         guard unbalancedTryLock() else { return nil }
         defer { unbalancedUnlock() }
         return try work()
     }
-}
-
-/// A basic wrapper around the "NORMAL" and "RECURSIVE" `pthread_mutex_t` (a safe, general purpose FIFO mutex). This type is a "class" type to take advantage of the "deinit" method and prevent accidental copying of the `pthread_mutex_t`.
-public final class PThreadMutex: RawMutex {
-    public typealias MutexPrimitive = pthread_mutex_t
+    
+    typealias MutexPrimitive = pthread_mutex_t
     
     // Non-recursive "PTHREAD_MUTEX_NORMAL" and recursive "PTHREAD_MUTEX_RECURSIVE" mutex types.
-    public enum PThreadMutexType {
+    enum PThreadMutexType {
         case normal
         case recursive
     }
     
-    public var unsafeMutex = pthread_mutex_t()
+    var unsafeMutex = pthread_mutex_t()
     
     /// Default constructs as ".Normal" or ".Recursive" on request.
-    public init(type: PThreadMutexType = .normal) {
+    init(type: PThreadMutexType = .normal) {
         var attr = pthread_mutexattr_t()
         guard pthread_mutexattr_init(&attr) == 0 else {
             preconditionFailure()
@@ -118,18 +67,29 @@ public final class PThreadMutex: RawMutex {
         pthread_mutex_destroy(&unsafeMutex)
     }
     
-    public func unbalancedLock() {
+    func unbalancedLock() {
         pthread_mutex_lock(&unsafeMutex)
     }
     
-    public func unbalancedTryLock() -> Bool {
+    func unbalancedTryLock() -> Bool {
         return pthread_mutex_trylock(&unsafeMutex) == 0
     }
     
-    public func unbalancedUnlock() {
+    func unbalancedUnlock() {
         pthread_mutex_unlock(&unsafeMutex)
     }
 }
+
+
+//
+//  Lazy.swift
+//  Example
+//
+//  Created by Bell App Lab on 21.12.17.
+//  Copyright © 2017 Bell App Lab. All rights reserved.
+//
+
+import Foundation
 
 
 public class Lazy<Value>: CustomStringConvertible, CustomDebugStringConvertible
